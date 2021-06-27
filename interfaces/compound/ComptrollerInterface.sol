bdfd8c03b8c409566c07254a49c008cd65e95937
lohba <lukeohba@gmail.com>
2021-06-27 13:40:02 -0700

    claimComp

---
+++ interfaces/compound/ComptrollerInterface.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//it was supposed to be redeemAmount as a parameter for redeemUnderlying and uint paramters were made uint256
+pragma solidity ^0.6.11;
+
+/**
+ * @dev ComptrollerInterface contract interface.
+ * The ComptrollerInterface contract is available here
+ * github.com/compound-finance/compound-protocol/blob/master/contracts/ComptrollerInterface.sol.
+ */
+interface ComptrollerInterface {
+
+    function claimComp(address) external;
+
+}
